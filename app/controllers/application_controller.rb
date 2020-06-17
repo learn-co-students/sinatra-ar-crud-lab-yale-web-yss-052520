62c702c68f35b87e1f0e335f3ae436f211edf6ec
ZIming Mao <ziming.mao@yale.edu>
2020-06-16 23:26:26 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,47 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  set :method_override, true
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get '/' do
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do 
+    article = Article.create(params)
+    redirect('/articles/' + article.id.to_s)
+  end
+  get '/articles/new' do
+    erb :new
+  end
+  get '/articles/:id' do
+    @article = Article.all.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do 
+    @article = Article.all.find(params[:id])
+    erb :edit
+
+  end
+
+  patch '/articles/:id' do
+    article = Article.all.find(params[:id])
+    article.update(title: params[:title], content: params[:content])
+    redirect "/articles/#{article.id}"
+  end
+
+  delete '/articles/:id' do 
+    article = Article.all.find(params[:id])
+    article.destroy
+    redirect "/articles"
   end
 end
