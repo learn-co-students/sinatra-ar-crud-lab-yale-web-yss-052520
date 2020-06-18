73d483149f3a66965c358611d101bb2dd0b84aad
s-dney <littlebirdsword@gmail.com>
2020-06-18 10:28:33 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,50 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  post '/articles' do
+    article = Article.create(params)
+    redirect "/articles/#{article.id}"
+  end
+
+  patch '/articles/:id' do
+    #binding.pry
+    article = Article.find(params[:id])
+    article.update(title: params[:title], content: params[:content])
+    #binding.pry
+    redirect "/articles/#{article.id}"
+  end
+
+  delete '/articles/:id' do
+    #binding.pry
+    article = Article.find(params[:id])
+    article.destroy
+    redirect "/articles"
+  end
+
+
 end
--- config.ru
+++ config.ru
@@ -1,13 +1,9 @@
-
 require './config/environment'
 
 begin
   fi_check_migration
 
-  use Rack::MethodOverride
 
   run ApplicationController
-rescue ActiveRecord::PendingMigrationError => err
-  STDERR.puts err
-  exit 1
+
 end
