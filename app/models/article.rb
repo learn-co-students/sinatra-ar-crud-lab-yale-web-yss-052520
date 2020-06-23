e183e2c482854355a990152b120ee85ca0655f94
KalebGz <kaleb.zeray@gmail.com>
2020-06-22 22:18:48 -0400

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+#     @@all << self
+#     def initialize(title:, content:)
+#         @title = title
+#         @content = content
+#     end
+
+# end
 end
